37149a9f1c6d8f593a442f49c755d43f1b88d2a1
Lucas Eduardo <lucaseduardocrp@gmail.com>
2023-04-05 20:39:16 -0300

    new portfolio project

--- assets/js/main.js
+++
@@ -1,5 +0,0 @@
-var typed = new Typed(".typed", {
-  strings: ["A combinação de Design, Desenvolvimento Frontend e habilidade <br> profissional para impulsionar sua marca no mercado."],
-  typeSpeed: 100,
-  loop: false
-});
