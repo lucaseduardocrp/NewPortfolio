f9238b59d1e828f768bce626c119f8e6478d9677
Lucas Eduardo <lucaseduardocrp@gmail.com>
2023-04-07 16:19:00 -0300

    new portfolio update

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+// const header = document.querySelector("header");
+
+// window.addEventListener ("scroll", function() {
+//     header.classList.toggle ("sticky", window.scrollY > 0);
+// });
+
+let menu = document.querySelector('.mobile-menu');
+let navbar = document.querySelector('.nav-bar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('open');
+};
+
+const sr = ScrollReveal ({
+    distance: '25px',
+    duration: 2500,
+    reset: true
+})
+
+sr.reveal('.home',{delay:190, origin: 'bottom'})
+
+sr.reveal('.about,.skills,.service,.project,.contact',{delay: 200, origin: 'right'})
