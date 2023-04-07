23ddb4ac98293b4be1d936baab909b424954fc52
Lucas Eduardo <lucaseduardocrp@gmail.com>
2023-04-07 16:38:25 -0300

    new portfolio update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,30 @@
-// const header = document.querySelector("header");
+const header = document.querySelector("header");
+const nav = document.querySelector("nav");
 
-// window.addEventListener ("scroll", function() {
-//     header.classList.toggle ("sticky", window.scrollY > 0);
-// });
+window.addEventListener ("scroll", function() {
+    header.classList.toggle ("sticky", window.scrollY > 0);
+    nav.classList.toglle("sticky-nav", window.scrolY > 0)
+});
 
-let menu = document.querySelector('.mobile-menu');
-let navbar = document.querySelector('.nav-bar');
+// let menu = document.querySelector('.mobile-menu');
+// let navbar = document.querySelector('.nav-bar');
 
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');
-};
+// menu.onclick = () => {
+//     menu.classList.toggle('bx-x');
+//     navbar.classList.toggle('open');
+// };
 
-window.onscroll = () => {
-    menu.classList.remove('bx-x');
-    navbar.classList.remove('open');
-};
+// window.onscroll = () => {
+//     menu.classList.remove('bx-x');
+//     navbar.classList.remove('open');
+// };
 
-const sr = ScrollReveal ({
-    distance: '25px',
-    duration: 2500,
-    reset: true
-})
+// const sr = ScrollReveal ({
+//     distance: '25px',
+//     duration: 2500,
+//     reset: true
+// })
 
-sr.reveal('.home',{delay:190, origin: 'bottom'})
+// sr.reveal('.home',{delay:190, origin: 'bottom'})
 
-sr.reveal('.about,.skills,.service,.project,.contact',{delay: 200, origin: 'right'})
+// sr.reveal('.about,.skills,.service,.project,.contact',{delay: 200, origin: 'right'})
