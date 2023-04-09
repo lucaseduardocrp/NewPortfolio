26c211125544215f5d02d6e97cca0a138682813f
Lucas Eduardo <lucaseduardocrp@gmail.com>
2023-04-09 00:18:54 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,22 @@
+const header = document.querySelector("header");
+
 window.onload = function(){
     AOS.init({
-        duration: 2000,
+        duration: 1500,
     });
 }
+
+let menu = document.querySelector('.mobile-menu');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('open');
+};
+
 
